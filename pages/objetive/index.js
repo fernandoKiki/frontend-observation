212a03341e9c221503de3e0ea180d010ebd7d9cc
Eric Janero <ejanerop97@gmail.com>
2021-11-02 22:46:10 -0400

    getStaticProp in objective index

--- pages/objetive/index.js
+++ pages/objetive/index.js
@@ -1,10 +1,28 @@
 import Layout from "../../components/layout";
 import Title from "../../components/title";
 
-export default function Objetive(){
+export default function Objetive() {
     return(
         <Layout>
-            <Title>Objetive Info</Title>
+            <Title>Lista de objetivos</Title>
+            <ul>
+                {objetives.map((objetive) => (
+                    <li key={objetive.id}>{objetive.title}</li>
+                ))}
+            </ul>
         </Layout>
     )
 }
+
+export async function getStaticProps() {
+    const res = await fetch('https://.../objetives') // url del backend, de la lista de objetivos, debe ser http://localhost:3000/objetives
+    const posts = await res.json()
+  
+    return {
+      props: {
+        objetives,
+      },
+    }
+  }
+  
+
