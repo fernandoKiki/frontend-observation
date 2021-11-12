60856588d252d263ed1aa75824aa0c65e63973da
fernandoKiki <kikihildy@gmail.com>
2021-11-12 10:32:39 -0500

    getAll conection

--- pages/observation/index.js
+++ pages/observation/index.js
@@ -1,10 +1,35 @@
+
 import Layout from "../../components/layout";
 import Title from "../../components/title";
-
-export default function Observation(){
+import Link from "next/dist/client/link";
+export default function Observation({ observation }){
     return(
         <Layout>
-            <Title>Observation Info</Title>
+            <Title>Observation List</Title>
+            <div>
+               {observation.map(observation =>{
+                   return(
+                       <Link></Link>
+                   )
+               })}
+            </div>
+           
         </Layout>
     )
 }
+<ul>
+{observation.map((observation) => (
+    <li key={observation.id}>{observation.name}</li>
+))}
+</ul>
+
+export async function getStaticProps() {
+    const res = await fetch('http://localhost:3000/observation') // url del backend
+    const observation = await res.json()
+  
+    return {
+      props: {
+        observation,
+      },
+    }
+  }
